97475fa971e4730c0dccf84c281dc9943d251e61
AntonMosen <antonmosenbacher@hotmail.com>
2019-05-11 18:13:25 +0200

    Added ID. Closes #12.

--- src/main/java/com/springboot/playground/entity/Location.java
+++ src/main/java/com/springboot/playground/entity/Location.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
@@ -14,6 +17,9 @@ import javax.persistence.Entity;
 @Builder
 public class Location {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String country;
     private String cityname;
     private String streetname;
