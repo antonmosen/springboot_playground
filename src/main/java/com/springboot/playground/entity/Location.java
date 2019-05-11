98cba27f36c5e45679fefb16fd0723bc15aa2460
AntonMosen <antonmosenbacher@hotmail.com>
2019-05-11 18:00:30 +0200

    Added Builder to Location Entity. Closes #10.

--- src/main/java/com/springboot/playground/entity/Location.java
+++ src/main/java/com/springboot/playground/entity/Location.java
@@ -1,11 +1,17 @@
 package com.springboot.playground.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Location {
 
     private String country;
