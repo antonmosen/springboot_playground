169a6526e8c30daf0816508ef096f6b9ab4fd981
AntonMosen <antonmosenbacher@hotmail.com>
2019-05-11 19:16:22 +0200

    Created Person service and implementation. Closes #16.

---
+++ src/main/java/com/springboot/playground/service/PersonService.java
@@ -0,0 +1,5 @@
+package com.springboot.playground.service;
+
+public interface PersonService {
+
+}
---
+++ src/main/java/com/springboot/playground/service/PersonServiceImpl.java
@@ -0,0 +1,8 @@
+package com.springboot.playground.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+}
