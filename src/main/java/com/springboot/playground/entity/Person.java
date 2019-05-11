ed81aaaf3debcf72f2854ecf9605ce4c7c1089dd
AntonMosen <antonmosenbacher@hotmail.com>
2019-05-11 18:12:49 +0200

    Added ID. Closes #11.

--- src/main/java/com/springboot/playground/entity/Person.java
+++ src/main/java/com/springboot/playground/entity/Person.java
@@ -6,8 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -16,6 +15,9 @@ import javax.persistence.OneToOne;
 @Builder
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String firstname;
     private String lastname;
     private Integer age;
