a07ffe9a0c443556bef0779fcf62489069225cd8
AntonMosen <antonmosenbacher@hotmail.com>
2019-05-11 17:59:36 +0200

    Added Builder to Person Entity. Closes #9.

--- src/main/java/com/springboot/playground/entity/Person.java
+++ src/main/java/com/springboot/playground/entity/Person.java
@@ -1,13 +1,19 @@
 package com.springboot.playground.entity;
 
 import com.springboot.playground.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Person {
 
     private String firstname;
