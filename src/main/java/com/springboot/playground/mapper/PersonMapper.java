6d72a68724ac94549283ccf7b2b594b4dee1790f
AntonMosen <antonmosenbacher@hotmail.com>
2019-05-12 21:16:26 +0200

    Refactored mapper. Closes #37.

--- src/main/java/com/springboot/playground/mapper/PersonMapper.java
+++ src/main/java/com/springboot/playground/mapper/PersonMapper.java
@@ -11,16 +11,18 @@ public interface PersonMapper {
 
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
+    @Mapping(target = "location", ignore = true)
+    @Mapping(target = "id", ignore = true)
     @Mapping(source = "personDto.locationDto.country", target = "location.country")
     @Mapping(source = "personDto.locationDto.cityname", target = "location.cityname")
     @Mapping(source = "personDto.locationDto.streetname", target = "location.streetname")
     @Mapping(source = "personDto.locationDto.houseNumber", target = "location.houseNumber")
     Person personDtoToPerson(PersonDto personDto);
 
+    @Mapping(target = "locationDto", ignore = true)
     @Mapping(source = "person.location.country", target = "locationDto.country")
     @Mapping(source = "person.location.cityname", target = "locationDto.cityname")
     @Mapping(source = "person.location.streetname", target = "locationDto.streetname")
     @Mapping(source = "person.location.houseNumber", target = "locationDto.houseNumber")
     PersonDto personToPersonDto(Person person);
-
 }
