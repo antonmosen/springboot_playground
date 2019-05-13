0ee049ccfc25a3b6ba8ed0c7b9a43779fd253e30
AntonMosen <antonmosenbacher@hotmail.com>
2019-05-13 20:05:33 +0200

    Added header validation. Closes #39.

--- README.md
+++ README.md
@@ -8,7 +8,12 @@ Helpful urls for actuator, will be updated in time.
 * http://localhost:8080/actuator/health
 
 ## H2 database
-Use http://localhost:8080/h2-console to access console.
+To use H2 console in chrome, or other browsers, you must send a header with your request.
+for Chrome, download ModHeader extension and add X-Trace and X-Value as header.
+Use http://localhost:8080/h2-console to access console. 
 
 ## Postman
-In order to send request, you must add firstname and lastname as header.
+In order to send request, you must add X-Trace and X-Value as header.
+The values are stored in application.properties.
+Everytime a someone consumes the api, the ApiHeaderFilter kicks in and validates the header.
+If no valid header, they get unauthorized as response.
--- src/main/java/com/springboot/playground/header/ApiHeaderFilter.java
+++ src/main/java/com/springboot/playground/header/ApiHeaderFilter.java
@@ -1,6 +1,10 @@
 package com.springboot.playground.header;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -12,32 +16,55 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Configuration
+@Getter
+@Setter
+@PropertySource("classpath:application.properties")
 public class ApiHeaderFilter extends GenericFilterBean {
+
+    @Value("${header.parameter.app.x.trace}")
+    private String headerParameterAppXTrace;
+    @Value("${header.parameter.app.x.value}")
+    private String headerParameterAppValue;
+
+    @Value("${app.x.trace}")
+    private String appXTrace;
+    @Value("${app.x.value}")
+    private String appXValue;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        String FIRST_NAME = "firstname";
-        String LAST_NAME = "lastname";
-
-        if (isNullOrEmpty(request.getHeader(FIRST_NAME))) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No valid header parameter : "+ FIRST_NAME);
+        if (isNotValidCredentials(request.getHeader(headerParameterAppXTrace), appXTrace)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No valid header parameter : " + headerParameterAppXTrace);
             return;
         }
 
-        if (isNullOrEmpty(request.getHeader(LAST_NAME))) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No valid header parameter : "+ LAST_NAME);
+        if (isNotValidCredentials(request.getHeader(headerParameterAppValue), appXValue)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No valid header parameter : " + headerParameterAppValue);
             return;
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
-    private boolean isNullOrEmpty(String value) {
-        if (value == null) {
+    private boolean isNotValidCredentials(String headerValue, String appValue) {
+
+        if (headerValue == null || appValue == null) {
             return true;
         }
-        return "".equals(value);
+        if ("".equals(headerValue) || "".equals(appValue)) {
+            return true;
+        }
+        return !headerValue.equals(appValue);
     }
 }
+
+
+
+
+
+
+
+
+
